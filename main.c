bcee60bb2eaf40d170001fa808d211199b647973
EmileBrt <bertelootemile@gmail.com>
2024-12-07 23:08:23 +0100

    c03 done

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include "ft.h"
 
 int main(){
-    char test_str[] = "Bonjour comment allez vous \0";
-    
-    // Print the address as a hexadecimal value
-    printf("Address of pointer (hex): %p\n", (void *)test_str);
+    char a[100] = "Hello World Hello";
+    char *b = "World";
 
-    // Print the address as an unsigned long
-    printf("Address of pointer (unsigned long): %lu\n", (unsigned long)test_str);
+    printf("res : %s \n", ft_strstr(a,b));
 
-    ft_print_memory(&test_str,sizeof(test_str));
     return 0;
 }
