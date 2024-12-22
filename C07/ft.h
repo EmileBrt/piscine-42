19aa877614f9d2879efa5f153e46b8c9cf08b953
EmileBrt <bertelootemile@gmail.com>
2024-12-22 16:55:10 +0100

    day 7 done

--- C07/ft.h
+++ C07/ft.h
@@ -95,10 +95,77 @@ char *ft_strjoin(int size, char **strs, char *sep) {
 //C07-04 BOORRING
 
 //C07-05
-char **ft_split(char *str, char *charset){
-    int size_str = ft_strlen(str);
-    int nbr_res = 0;
-    for(int i =0; i < (size_str - 1); i++ ){
-        
+int is_separator(char c, char *charset) {
+    while (*charset) {
+        if (c == *charset) {
+            return 1;
+        }
+        charset++;
     }
-};
+    return 0;
+}
+
+int count_substrings(char *str, char *charset) {
+    int count = 0;
+    int in_word = 0;
+
+    while (*str) {
+        if (!is_separator(*str, charset)) {
+            if (!in_word) {
+                in_word = 1;
+                count++;
+            }
+        } else {
+            in_word = 0;
+        }
+        str++;
+    }
+    return count;
+}
+
+char *copy_substring(char *start, char *end) {
+    int length = end - start;
+    char *substring = (char *)malloc((length + 1) * sizeof(char));
+    if (!substring) {
+        return NULL;
+    }
+    for (int i = 0; i < length; i++) {
+        substring[i] = start[i];
+    }
+    substring[length] = '\0';
+    return substring;
+}
+
+char **ft_split(char *str, char *charset) {
+    int count = count_substrings(str, charset);
+    char **result = (char **)malloc((count + 1) * sizeof(char *));
+    if (!result) {
+        return NULL;
+    }
+
+    int index = 0;
+    char *start = str;
+    int in_word = 0;
+
+    while (*str) {
+        if (!is_separator(*str, charset)) {
+            if (!in_word) {
+                in_word = 1;
+                start = str;
+            }
+        } else {
+            if (in_word) {
+                in_word = 0;
+                result[index++] = copy_substring(start, str);
+            }
+        }
+        str++;
+    }
+
+    if (in_word) {
+        result[index++] = copy_substring(start, str);
+    }
+
+    result[index] = NULL;
+    return result;
+}
--- C07/main.c
+++ C07/main.c
@@ -2,12 +2,15 @@
 #include "ft.h"
 
 int main() {
-    char *strs[] = {
-        "yes",
-        "no"
-    };
-    char *res = ft_strjoin(2, strs, "!");
-    printf("res : %s\n", res);
-    free(res);
+    char *str = "Hello, world! This is a test.";
+    char *charset = " ,!.";
+    char **result = ft_split(str, charset);
+
+    for (int i = 0; result[i] != NULL; i++) {
+        printf("%s\n", result[i]);
+        free(result[i]);
+    }
+    free(result);
+
     return 0;
 }
