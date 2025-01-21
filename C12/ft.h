3125943710b45e0e61cde642bac3bef6db2e3cbb
EmileBrt <bertelootemile@gmail.com>
2025-01-21 18:09:22 +0100

    day 12

--- C12/ft.h
+++ C12/ft.h
@@ -11,5 +11,29 @@ t_list *ft_create_elem(void *data){
 };
 
 void ft_list_push_front(t_list **begin_list, void *data){
-    
+    t_list * new = ft_create_elem(data);
+    new->next = *begin_list;
+    *begin_list = new;    
+};
+
+int ft_list_size(t_list *begin_list){
+    t_list * temp = begin_list;
+    int c;
+    while(temp != NULL){
+        c ++;
+        temp = temp->next;
+    }
+    return c;
+};
+
+t_list *ft_list_last(t_list *begin_list){
+    t_list * temp = begin_list;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    return temp;
+};
+
+void ft_list_push_back(t_list **begin_list, void *data){
+
 };
--- C12/main.c
+++ C12/main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ft.h"
 
-int main() {
-    t_list* res = ft_create_elem("azer");
+int main()
+{
+    t_list *list = NULL;
+    int data1 = 10;
+    int data2 = 20;
 
-    printf("data : %s \n", (char*)res->data);
-    
+    ft_list_push_front(&list, &data1);
+    ft_list_push_front(&list, &data2);
+    ft_list_push_front(&list, &data2);
+    ft_list_push_front(&list, &data2);
+    ft_list_push_front(&list, &data2);
+    ft_list_push_front(&list, &data2);
+    ft_list_push_front(&list, &data2);
+    ft_list_push_front(&list, &data2);
+
+    printf("size list : %i \n", ft_list_size(list));
+
     return 0;
 }
